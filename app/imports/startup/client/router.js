7889b3345c654ef5fc2c7a8cfede07201fc6aa5e
Christopher Nguyen <cnguyen7@hawaii.edu>
2017-04-08 00:53:24 -1000

    Made uVenture template

---
+++ app/imports/startup/client/router.js
@@ -0,0 +1,36 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+  name: 'Home_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Home_Page' });
+  },
+});
+
+FlowRouter.route('/find', {
+  name: 'Find_Stuff_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Find_Adventure_Page' });
+  },
+});
+
+FlowRouter.route('/add', {
+  name: 'Add_Adventure_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Adventure_Page' });
+  },
+});
+
+FlowRouter.route('/edit', {
+  name: 'Edit_Adventure_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Adventure_Page' });
+  },
+});
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
+  },
+};
---
+++ app/imports/ui/pages/index.js
@@ -0,0 +1,13 @@
+import './home-page.html';
+
+import './add-adventure-page.html';
+
+
+import './find-adventure-page.html';
+
+
+import './edit-adventure-page.html';
+
+import './edit-contact-page.html';
+import './edit-contact-page.js';
+
