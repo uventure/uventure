e9dc5db9c598abdf16e922a3d74fd4ad99d48ab8
Christopher Nguyen <cnguyen7@hawaii.edu>
2017-04-12 22:32:50 -1000

    Edited settings for Galaxy and Stuff

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -22,15 +22,14 @@ if (!Meteor.settings.cas) {
 /* eslint-disable no-console */
 
 /* When running app for first time, pass a settings file to set up a default user account.
-if (Meteor.users.find().count() === 0) {
-  if (!!Meteor.settings.defaultAccount) {
-    Accounts.createUser({
-      username: Meteor.settings.defaultAccount.username,
-      password: Meteor.settings.defaultAccount.password,
-    });
-  } else {
-    console.log('No default user!  Please invoke meteor with a settings file.');
-  }
-}
-*/
-
+ if (Meteor.users.find().count() === 0) {
+ if (!!Meteor.settings.defaultAccount) {
+ Accounts.createUser({
+ username: Meteor.settings.defaultAccount.username,
+ password: Meteor.settings.defaultAccount.password,
+ });
+ } else {
+ console.log('No default user!  Please invoke meteor with a settings file.');
+ }
+ }
+ */
