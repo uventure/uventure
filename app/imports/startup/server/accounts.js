01cadd49576bd709a0e7c5b7a1c6e77e4df66d00
Christopher Nguyen <cnguyen7@hawaii.edu>
2017-04-13 00:12:19 -1000

    Made mockup CAS for presentation

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -1,34 +1,40 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { _ } from 'meteor/underscore';
+
+//import { Meteor } from 'meteor/meteor';
+//import { Accounts } from 'meteor/accounts-base';
+//import { _ } from 'meteor/underscore';
 
 /* eslint-disable no-console */
 
 /* Validate username, sending a specific error message on failure. */
-Accounts.validateNewUser(function (user) {
-  if (user) {
-    const username = user.services.cas.id;
-    if (username && _.contains(Meteor.settings.allowed_users, username)) {
-      return true;
-    }
-  }
-  throw new Meteor.Error(403, 'User not in the allowed list');
-});
+//Accounts.validateNewUser(function (user) {
+  //if (user) {
+    //const username = user.services.cas.id;
+    //if (username && _.contains(Meteor.settings.allowed_users, username)) {
+     // return true;
+    //}
+  //}
+ // throw new Meteor.Error(403, 'User not in the allowed list');
+//});
 
 
-if (!Meteor.settings.cas) {
-  console.log('CAS settings not found! Hint: "meteor --settings ../config/settings.development.json"');
-}
+//if (!Meteor.settings.cas) {
+ // console.log('CAS settings not found! Hint: "meteor --settings ../config/settings.development.json"');
+//}
+
+
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
 /* eslint-disable no-console */
 
-
- if (Meteor.users.find().count() === 0) {
- if (!!Meteor.settings.defaultAccount) {
- Accounts.createUser({
- username: Meteor.settings.defaultAccount.username,
- password: Meteor.settings.defaultAccount.password,
- });
- } else {
- console.log('No default user!  Please invoke meteor with a settings file.');
- }
- }
+/* When running app for first time, pass a settings file to set up a default user account. */
+if (Meteor.users.find().count() === 0) {
+  if (!!Meteor.settings.defaultAccount) {
+    Accounts.createUser({
+      username: Meteor.settings.defaultAccount.username,
+      password: Meteor.settings.defaultAccount.password,
+    });
+  } else {
+    console.log('No default user!  Please invoke meteor with a settings file.');
+  }
+}
